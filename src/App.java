9386e4bc1f8018a86ca1d8e3d12e0e23e282f7dd
ivandanque <ralphivan.danque.cics@ust.edu.ph>
2022-12-09 21:00:52 +0800

    Changed codes and Added Task

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+1);
+        }
     }
 }
