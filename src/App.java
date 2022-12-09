943bd843532234ab5eaaa3b8c136b56809b8f0e5
cassandra0129 <princesscassandradelrosario@gmail.com>
2022-12-09 21:40:11 +0800

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println(i+2);
         }
     }
